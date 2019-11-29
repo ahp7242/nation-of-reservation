c3c4ee2870537e6726b465ec6ea163644ad65555
GD3 <GD3@DESKTOP-SL3939K>
2019-11-29 17:35:41 +0900

    <정화, 정우> customer.html완성시켜 고객이 pc방 좌석을 확인할 수 있다. templet을 제작하여 index.html화면 제작 및 각각의 html에 적용 login.html 제작

--- nor/src/main/java/com/picaboo/nor/customer/controller/CustomerController.java
+++ nor/src/main/java/com/picaboo/nor/customer/controller/CustomerController.java
@@ -1,6 +1,5 @@
 package com.picaboo.nor.customer.controller;
  
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,20 +7,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import com.picaboo.nor.customer.service.CustomerService;
-import com.picaboo.nor.customer.vo.Seat;
+import com.picaboo.nor.customer.service.*;
+import com.picaboo.nor.customer.vo.*;
 
 @Controller
 public class CustomerController {
 	@Autowired private CustomerService customerService;
 	
-	@GetMapping("/")
-	public String index(Model model) {
+	@GetMapping("/customer")
+	public String customer(Model model) {
 		String franchiseeNo = "1";
 		List<Seat> seat = customerService.getSeat(franchiseeNo);
 		
 		model.addAttribute("seat", seat);
 		//System.out.println("cnt :"+seat);
 		return "customer";
+	}
+	
+	@GetMapping({"/","/index"})
+	public String index() {
+		return "index";
+	}
+	
+	@GetMapping("/login")
+	public String login() {
+		return "login";
 	}
 }
