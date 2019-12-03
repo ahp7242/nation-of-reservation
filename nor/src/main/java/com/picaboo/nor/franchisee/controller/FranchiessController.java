910289bab93dfcd68c7699d07c99da1eb7d2611f
GD3 <GD3@DESKTOP-KK3U52K>
2019-12-03 10:35:14 +0900

    가맹점 등록 페이지 추가
    1. franchisee/addFranchisee.html 추가
    2. 좌석 정보에 가맹점 번호를 연결하기 위해 /franchisee/vo/Seat.java에 franchisee_no 추가

--- nor/src/main/java/com/picaboo/nor/franchisee/controller/FranchiessController.java
+++ nor/src/main/java/com/picaboo/nor/franchisee/controller/FranchiessController.java
@@ -14,9 +14,14 @@ import com.picaboo.nor.franchisee.service.FranchiseeService;
 public class FranchiessController {
 	@Autowired FranchiseeService franchiseeService;
 	
+	@GetMapping("/addFranchisee")
+	public String addFranchisee() {
+		return "addFranchisee";
+	}
+	
 	// 가맹점 좌석 입력 요청
 	@PostMapping("/addFranchiseeSeat")
-	public String franchiseeAddSeat( @RequestParam() HashMap<String,String> seatMap) {
+	public String addFranchiseeSeat( @RequestParam() HashMap<String,String> seatMap) {
 		System.out.println("addFranchiseeSeat Post 요청");
 		
 		System.out.println("addFranchiseeSeat.seatList: " + seatMap);
@@ -27,7 +32,7 @@ public class FranchiessController {
 	
 	// 가맹점 좌석 추가 페이지 요청
 	@GetMapping("/addFranchiseeSeat") 
-	public String franchiseeAddSeat() {
+	public String addFranchiseeSeat() {
 		System.out.println("addFranchiseeSeat Get 요청");
 		return "addFranchiseeSeat";
 	}
